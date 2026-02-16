bb725f01b3791a9d0b0fa73cb27416ca395f8ccc
ARVIND <arvindpuri1492@gmail.com>
2026-02-16 20:41:54 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p resource
+
+touch src/__init__.py
+touch src/helper.py
+touch src/promp.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+echo "Files created"
+
